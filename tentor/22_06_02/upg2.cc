35e861b542248a5c5ce0588839c21378a4b61a29
Stian Lockhart Pedersen <stilo759@student.liu.se>
2022-10-12 13:10:20 +0200

    yy

--- tentor/22_06_02/upg2.cc
+++ tentor/22_06_02/upg2.cc
@@ -8,14 +8,12 @@ public:
     virtual ~Variable_Base() = default;
     virtual void print(std::ostream &os) = 0;
     virtual Variable_Base *add(Variable_Base *rhs) = 0;
-
-private:
 };
 
 class Number : public Variable_Base
 {
 public:
-    Number(double d) : value{d} {};
+    Number(double const &d) : value{d} {};
     void print(std::ostream &os) override
     {
         os << value;
@@ -28,18 +26,18 @@ public:
         }
         else
         {
-            throw std::invalid_argument("what");
+            throw std::invalid_argument("Invalid");
         }
     }
 
 private:
-    double value{};
+    double const value{};
 };
 
 class String : public Variable_Base
 {
 public:
-    String(std::string s) : value{s} {};
+    String(std::string const &s) : value{s} {};
     void print(std::ostream &os) override
     {
         os << value;
@@ -65,8 +63,45 @@ public:
     }
 
 private:
-    std::string value;
+    std::string const value;
 };
+
+class Variable
+{
+public:
+    Variable(double const &d) : data{new Number{d}} {};
+    Variable(std::string const &s) : data{new String{s}} {};
+    Variable(Variable_Base *vb) : data{vb} {};
+
+    Variable operator+(Variable &rhs)
+    {
+        return Variable{data->add(rhs.data)};
+    };
+
+    Variable &operator=(double const &rhs)
+    {
+        delete data;
+        data = new Number{rhs};
+        return *this;
+    };
+    Variable &operator=(std::string const &rhs)
+    {
+        delete data;
+        data = new String{rhs};
+        return *this;
+    };
+
+    friend std::ostream &operator<<(std::ostream &os, Variable const vb);
+
+private:
+    Variable_Base *data;
+};
+
+std::ostream &operator<<(std::ostream &os, Variable const vb)
+{
+    vb.data->print(os);
+    return os;
+}
 
 /* Expected output:
 
@@ -78,22 +113,22 @@ Unknown operator
  */
 int main()
 {
-    // Variable var1{"hello"};
-    // Variable var2{5.3};
-    // Variable var3{"world"};
-    // Variable var4{1.2};
+    Variable var1{"hello"};
+    Variable var2{5.3};
+    Variable var3{"world"};
+    Variable var4{1.2};
 
-    // std::cout << var1 + var2 << std::endl;
-    // std::cout << var1 + var3 << std::endl;
+    std::cout << var1 + var2 << std::endl;
+    std::cout << var1 + var3 << std::endl;
 
-    // try
-    // {
-    //     std::cout << var2 + var3 << std::endl;
-    // }
-    // catch (std::exception &e)
-    // {
-    //     std::cerr << e.what() << std::endl;
-    // }
+    try
+    {
+        std::cout << var2 + var3 << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 
-    // std::cout << var2 + var4 << std::endl;
+    std::cout << var2 + var4 << std::endl;
 }
